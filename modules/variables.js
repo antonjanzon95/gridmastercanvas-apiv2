121acbaa330dd11bdd6820c9feb445c5fa58cea4
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-04-28 09:48:01 +0200

    increase max player amount

--- modules/variables.js
+++ modules/variables.js
@@ -4,18 +4,24 @@ let GLOBAL_USERS = [];
 const GAME_RUNTIME_SECONDS = 5;
 
 const GAME_COLORS = [
-  "red",
-  "blue",
-  "green",
-  // "yellow",
-  // "purple",
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'purple',
   // "black",
-  // "pink",
-  // "teal",
+  'pink',
+  'teal',
   // "orange",
   // "gold",
 ];
 
 const MAX_USERS = GAME_COLORS.length;
 
-module.exports = { ROOMS, GAME_COLORS, MAX_USERS, GLOBAL_USERS, GAME_RUNTIME_SECONDS };
+module.exports = {
+  ROOMS,
+  GAME_COLORS,
+  MAX_USERS,
+  GLOBAL_USERS,
+  GAME_RUNTIME_SECONDS,
+};
